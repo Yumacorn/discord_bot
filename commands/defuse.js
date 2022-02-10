26ff0e6f9f27098568fa5d9b22134b6f5ce6183f
Ryan Mui <ReachRyanMui@gmail.com>
2022-02-09 21:20:06 -0500

    Started Discord.js community guide to create a server bot

---
+++ commands/defuse.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('defuse')
+        .setDescription('Replies with Defuse timer'),
+    async execute(interaction) {
+        await interaction.reply(`Defuse timer is a total of 40 seconds:\n1st Phase - 20 seconds - 1 beep per second\n2nd Phase - 10 seconds - 2 beeps per second\n3rd Phase - 5 seconds - 4 beeps per second\n4th Phase - 5 seconds - 8 beeps per second`);
+    },
+};
